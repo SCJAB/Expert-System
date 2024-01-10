57a390a5ecba7a618e9abf6a0f904e0b0e5a2a7b
SCJAB <zzep96@gmail.com>
2024-01-10 22:56:04 +0800

    app.vue

--- front-end/app.vue
+++ front-end/app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="app">
+    <form @submit.prevent="submitForm">
+      <div v-for="question in questions" :key="question.id">
+        <p>{{ question.question }}</p>
+        <div v-for="option in question.options" :key="option.id">
+          <label>
+            <input type="radio" :name="'selectedAnswers[' + question.id + ']'" :value="option.id" v-model="selectedAnswers[question.id]">
+            {{ option.option }}
+          </label><br>
+        </div>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </template>
