61e290dd622ff5e79f8229be752223ec93af0ea6
SCJAB <zzep96@gmail.com>
2024-01-13 22:54:41 +0800

    front-end

---
+++ front-end/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li><nuxt-link to="/">Home</nuxt-link></li>
+                    <li><nuxt-link to="/about">About</nuxt-link></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div>
+            <slot />
+        </div>
+
+        <footer>
+            <div></div>
+        </footer>
+    </div>
+  </template>
