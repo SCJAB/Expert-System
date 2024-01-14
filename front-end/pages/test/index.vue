4c6bb71bab87d6bef28da8b634e35659fa72ea99
SCJAB <zzep96@gmail.com>
2024-01-14 15:54:05 +0800

    front-end

--- front-end/pages/test/index.vue
+++ front-end/pages/test/index.vue
@@ -1,32 +1,44 @@
 <template>
-  <div>
-    <h1>Questions</h1>
-    <ul>
-      <li v-for="question in questions" :key="question.id">
-        {{ question.question }}
-        <ul>
-          <p v-for="option in question.options" :key="option.id">
-            <label>
-              <input type="radio" :name="'question_' + question.id" :value="option.id" v-model="selectedOptions[question.id]">
-              {{ option.option }}
-            </label>
-          </p>
-        </ul>
-      </li>
-    </ul>
+  <div class="flex justify-center items-center h-[89vh]">
+    <div>
+      <ul :key="currentQuestionIndex">
+        <li v-for="(question, index) in questions" :key="question.id" v-show="index === currentQuestionIndex">
+          <h1>Question: {{ question.id }}</h1>
+          {{ question.question }}
+          <ul>
+            <p v-for="option in question.options" :key="option.id">
+              <label>
+                <input type="radio" :name="'question_' + question.id" :value="option.id" v-model="selectedOptions[question.id]">
+                {{ option.option }}
+              </label>
+            </p>
+          </ul>
+        </li>
+      </ul>
 
-    <button @click="submitAnswers">Submit Answers</button>
+      <div class="controls">
+        <button @click="prevQuestion" :disabled="currentQuestionIndex === 0">Previous</button>
+        <button @click="nextQuestion" :disabled="currentQuestionIndex === questions.length - 1">Next</button>
+        <button @click="submitAnswers">Submit Answers</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 
 const questions = ref([]);
 const selectedOptions = ref({});
+const currentQuestionIndex = ref(0);
+const transitionName = ref('slide-fade');
 
 onMounted(async () => {
   await fetchQuestions();
+});
+
+watch(currentQuestionIndex, (newIndex, oldIndex) => {
+  transitionName.value = newIndex > oldIndex ? 'slide-fade-left' : 'slide-fade-right';
 });
 
 async function fetchQuestions() {
@@ -40,21 +52,23 @@ async function fetchQuestions() {
   questions.value = data.questions;
 }
 
+function nextQuestion() {
+  if (currentQuestionIndex.value < questions.value.length - 1) {
+    currentQuestionIndex.value++;
+  }
+}
+
+function prevQuestion() {
+  if (currentQuestionIndex.value > 0) {
+    currentQuestionIndex.value--;
+  }
+}
+
 function submitAnswers() {
   console.log(selectedOptions.value);
 }
 </script>
 
-<!-- <template>
-  <div>
-    <p>Questions</p>
-  </div>
-</template>
+<style scoped>
 
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style> -->
+</style>
