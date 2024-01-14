91d2fbf1d874983c5df3df864167f13562a34039
Yourboiralph <hernandezralphb@gmail.com>
2024-01-15 00:40:37 +0800

    merge

---
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(TakersTableSeeder::class);
+        $this->call(AdminsTableSeeder::class);
+        $this->call(QuestionOptionsTableSeeder::class);
+        $this->call(DepressionTypesSeeder::class);
+        // $this->call(ResponseTableSeeder::class);
+    }
+}
