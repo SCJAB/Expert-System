e37c359466de944fb48e735de88a584b1f3c80d9
SCJAB <zzep96@gmail.com>
2024-01-14 13:39:55 +0800

    front-end

--- front-end/layouts/default.vue
+++ front-end/layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="bg-[#b9b9b8]">
         <header class="shadow-2xl fade-in">
-            <nav class="flex justify-between p-5 px-10">
-                <div class="h-16 w-16"><nuxt-link to="/"><img src="~/assets/img/untangled1.png" alt="Untangled"></nuxt-link></div>
-                <ul class="flex justify-evenly items-center w-56 border border-red">
-                    <li><nuxt-link to="/">Home</nuxt-link></li>
-                    <li><nuxt-link to="/about">About</nuxt-link></li>
-                    <li><nuxt-link to="/login">Login</nuxt-link></li>
-                </ul>
+            <nav class="flex justify-between p-3 px-16">
+                <nuxt-link to="/" class="h-16 w-16"><img src="~/assets/img/untangled1.png" alt="Untangled"></nuxt-link>
+                <div class="flex items-center">
+                    <ul class="flex justify-evenly w-56">
+                        <li><nuxt-link to="/" class="font-bold text-lg hover:text-[#B8764B] duration-500 button">Home</nuxt-link></li>
+                        <li><nuxt-link to="/about" class="font-bold text-lg hover:text-[#B8764B] duration-500 button">About</nuxt-link></li>
+                    </ul>
+                    <nuxt-link to="/login" class="p-2 font-bold text-lg text-white rounded bg-yellow-700 hover:bg-transparent hover:text-yellow-950 hover:scale-110 duration-500 button">Login</nuxt-link>
+                </div>
             </nav>
         </header>
 
